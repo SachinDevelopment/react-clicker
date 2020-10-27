acd893c41a7bc9e4bd9221fd9928dd59e9cf064d
SachinDevelopment <53714757+SachinDevelopment@users.noreply.github.com>
2020-10-26 22:53:43 -0400

    Update index.jsx

--- src/components/colors/index.jsx
+++ src/components/colors/index.jsx
@@ -1,26 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
+
 const instance = axios.create({
     baseURL: 'http://localhost:5000/',
 });
 
 
-function Colors(props) {
+function Colors() {
     const [colors, setColors] = useState([]);
     console.log(colors);
     useEffect(() => {
         instance.get(`/api/colors`)
             .then(res => {
                 setColors(res.data);
-                console.log("printing response", res);
             }).catch(error => {
-                console.log("printing errror", error);
               });
     }, [colors])
 
     return (
         <div>
-            {colors.map((color, index) => {
+            {colors.map((color) => {
                return <div>{'this is the color ' + color.color}</div>
             })}
         </div>
